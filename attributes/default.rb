d67db6ca0049481d6e5de16d2b89702fb0c26f2f
Eric Hanko <eric.hanko1@gmail.com>
2017-12-18 02:47:46 -0800

    change pool

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['vsts_agent']['version'] = 'latest'
 default['vsts_agent']['agent_name'] = node['hostname']
 default['vsts_agent']['account'] = 'office'
-default['vsts_agent']['agent_pool'] = 'Hosted macOS Preview'
+default['vsts_agent']['agent_pool'] = 'OXO Hub Eng Mac Pool'
 default['vsts_agent']['additional_environment'] = {}
 
 default['vsts_agent']['admin_user'] = 'vagrant'
