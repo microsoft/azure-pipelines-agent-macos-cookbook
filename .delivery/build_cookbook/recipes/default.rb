37f07044ac386d17f59e60eddf66af1e0a7df9a1
Eric Hanko <eric.hanko1@gmail.com>
2018-02-02 15:24:11 -0800

    remove delivery

--- .delivery/build_cookbook/recipes/default.rb
+++
@@ -1,6 +0,0 @@
-#
-# Cookbook:: build_cookbook
-# Recipe:: default
-#
-# Copyright:: 2017, Microsoft Inc., All Rights Reserved.
-include_recipe 'delivery-truck::default'
--- .delivery/build_cookbook/recipes/smoke.rb
+++
@@ -1,6 +0,0 @@
-#
-# Cookbook:: build_cookbook
-# Recipe:: smoke
-#
-# Copyright:: 2017, Microsoft Inc., All Rights Reserved.
-include_recipe 'delivery-truck::smoke'
