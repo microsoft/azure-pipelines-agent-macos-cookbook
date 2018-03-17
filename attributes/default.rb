9751c2791dded2f0e66124125da81c4571141eb2
Eric Hanko <eric.hanko1@gmail.com>
2018-03-17 16:50:03 -0700

    update default version to 1.131.0; remove macos and sudo dependencies

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,5 +5,5 @@ default['vsts_agent']['data_bag_item'] = 'build_agent'
 default['vsts_agent']['agent_name'] = node['hostname']
 default['vsts_agent']['account'] = 'americanhanko'
 default['vsts_agent']['agent_pool'] = "American Hanko's Agents"
-default['vsts_agent']['version'] = '2.129.0'
+default['vsts_agent']['version'] = '2.131.0'
 default['vsts_agent']['additional_environment'] = {}
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'eric.hanko1@gmail.com'
 license 'MIT'
 description 'A dedicated cookbook for configuring a VSTS build/release agent on macOS.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.1.3'
+version '1.1.4'
 chef_version '~> 13.0' if respond_to?(:chef_version)
 
 source_url 'https://github.com/americanhanko/vsts-agent-macos-cookbook'
@@ -12,5 +12,3 @@ issues_url 'https://github.com/americanhanko/vsts-agent-macos-cookbook/issues'
 
 depends 'homebrew', '~> 4.3.0'
 depends 'tar', '~> 2.1.0'
-depends 'macos', '~> 1.8.0'
-depends 'sudo', '~> 5.3.0'
