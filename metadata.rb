6c230eb996f5340ce3f4db315709f6933735a97a
Eric Hanko <eric.hanko1@gmail.com>
2018-04-10 11:59:28 -0700

    bump chef dependency to >= 13

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'eric.hanko1@gmail.com'
 license 'MIT'
 description 'A dedicated cookbook for configuring a VSTS build/release agent on macOS.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.2.5'
-chef_version '~> 13.0' if respond_to?(:chef_version)
+version '1.2.6'
+chef_version '>= 13.0' if respond_to?(:chef_version)
 
 source_url 'https://github.com/americanhanko/vsts-agent-macos-cookbook'
 issues_url 'https://github.com/americanhanko/vsts-agent-macos-cookbook/issues'
