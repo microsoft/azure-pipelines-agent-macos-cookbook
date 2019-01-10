4dc448890b9c8a907449e62dc161093b8e3149fa
Eric Hanko <eric.hanko1@gmail.com>
2019-01-09 17:00:03 -0800

    bump vers to 2.1.1

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'MIT'
 description 'A dedicated cookbook for configuring an Azure DevOps build or release agent on macOS.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 chef_version '>= 14.0' if respond_to?(:chef_version)
-version '2.1.0'
+version '2.1.1'
 
 supports 'mac_os_x'
 
