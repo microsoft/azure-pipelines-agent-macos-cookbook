9094ec696e77736eaa9219a9467e647a71ebd9d4
Eric Hanko <eric.hanko1@gmail.com>
2019-06-10 18:01:38 -0700

    chef-vault 3.1

--- metadata.rb
+++ metadata.rb
@@ -5,13 +5,13 @@ license 'MIT'
 description 'A dedicated cookbook for configuring an Azure DevOps build or release agent on macOS.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 chef_version '>= 14.0' if respond_to?(:chef_version)
-version '3.0.1'
+version '3.0.2'
 
 supports 'mac_os_x'
 
 source_url 'https://github.com/americanhanko/azure-pipelines-agent-macos-cookbook'
 issues_url 'https://github.com/americanhanko/azure-pipelines-agent-macos-cookbook/issues'
 
-depends 'chef-vault', '~> 3.0.0'
+depends 'chef-vault', '~> 3.1.0'
 depends 'homebrew', '~> 5.0.0'
 depends 'tar', '~> 2.0.0'
